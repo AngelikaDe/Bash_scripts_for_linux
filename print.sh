a6e8082a409b61a8411ad36cc5759a604aa0c507
Angelika <105379014+AngelikaDe@users.noreply.github.com>
2023-02-23 19:52:24 +0300

    Create print.sh

---
+++ print.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+choice="y|Y"
+if [ "$#" -lt 0 ]
+then 
+echo "error. exit"
+exit 1
+fi
+
+echo "$(sh main_2.sh)"
+
+file_name="$(date "+%d_%m_%Y_%H_%M_%S").ststus" 
+read -p "Do you want to save the information? [Y/N]" choice
+	if [[ $choice = y ]] || [[ $choice = Y ]] 
+		then
+		# echo "YAAAS"
+		# chmod 777 main_2.sh
+		#./main_2.sh 1> $file_name
+		exec 1> $file_name
+		echo "$(sh main_2.sh)" >&1
+	fi
