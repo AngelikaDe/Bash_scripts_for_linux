524f1d30e664ac9a973fd0726ec90a8895c9a3b6
Анжелика Демина <angelika@192.168.1.3>
2023-03-19 22:21:32 +0300

    develop

--- 02/main.sh
+++ 02/main.sh
@@ -11,11 +11,8 @@ echo "$(sh info.sh)"
 
 file_name="$(date "+%d_%m_%Y_%H_%M_%S").ststus" 
 read -p "Do you want to save the information? [Y/N]" choice
-	if [[ $choice = y ]] || [[ $choice = Y ]] 
+	if [ $choice = y ] || [ $choice = Y ] 
 		then
-		# echo "YAAAS"
-		# chmod 777 main_2.sh
-		#./main_2.sh 1> $file_name
 		exec 1> $file_name
 		echo "$(sh info.sh)" >&1
 	fi
