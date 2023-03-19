fe6f3463d72449408f7b391630b673fdf1801116
Анжелика Демина <angelika@192.168.1.3>
2023-03-20 02:06:32 +0300

    develop

--- 04/main.sh
+++ 04/main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source col.conf
-array_numb=()
-array_colors=()
+declare -a array_color=()
+declare -a array_numb=()
 count=0
 
 array_name_colors=(black white red green purple)
@@ -17,6 +17,9 @@ green_back="\033[102m"
 blue_back="\033[104m"
 purple_back="\033[105m"
 black_back="\033[40m"
+
+source info.sh
+export $(cut -d= -f1 info.sh)
 
 export column1_background1=$column1_background
 if [ -z ${column1_background1} ]; then 
@@ -42,10 +45,10 @@ if [ -z ${column2_font_color2} ]; then
 fi
 array_numb[3]=$column2_font_color2
 
-echo "${array_numb[@]}"
+# echo "${array_numb[@]}"
 
-echo "${array_numb[2]}"
-echo "${array_numb[3]}"
+# echo "${array_numb[2]}"
+# echo "${array_numb[3]}"
 
 for i in "${array_numb[@]}"
 do
@@ -59,9 +62,9 @@ do
             ;;
         4) array_color+=( "$blue" );
             ;;
-        5) array_color+=( "$purple" );
+        5) array_color+=( "$purple" )
             ;;
-        6) array_color+=( "$black" );
+        6) array_color+=( "$black" )
             ;;
         *) echo "UNKNOWN OPTION ${array_numb[i]}. End. Try again"
             exit 0
@@ -75,11 +78,11 @@ do
             ;;
         3) array_color+=( "$green_back" )
             ;;
-        4) array_color+=( "$blue_back" );
+        4) array_color+=( "$blue_back" )
             ;;
-        5) array_color+=( "$purple_back" );
+        5) array_color+=( "$purple_back" )
             ;;
-        6) array_color+=( "$black_back" );
+        6) array_color+=( "$black_back" )
             ;;
         *) echo "UNKNOWN OPTION ${i}. End. Try again"
             exit 0
@@ -88,16 +91,32 @@ do
     fi
     count=$((count+1))
 done
-echo "${array_name_colors[2]}"
-echo "${array_numb[@]}"
+# echo "${array_name_colors[2]}"
+# echo "${array_numb[@]}"
 echo ""
 color_1=${array_numb[0]}
 color_2=${array_numb[1]}
 color_3=${array_numb[2]}
 color_4=${array_numb[3]}
 
-echo "Column 1 background = ${array_numb[0]} ("${array_name_color[$color_1]}")"
-echo "YEES ${array_name_colors[$color_1]}"
-# echo "The value of the test variable is: ${column1_background}"
-# echo "${array_color[0]}${array_color[1]}HENLO $end=${array_color[2]}${array_color[3]} MY$end"
-# echo "NOOOO it is the worst"
+# echo "Column 1 background = ${array_numb[0]} ("${array_name_color[$color_1]}")"
+# echo "YEES ${array_name_colors[$color_1]}"
+# # echo "The value of the test variable is: ${column1_background}"
+# # echo "${array_color[0]}${array_color[1]}HENLO $end=${array_color[2]}${array_color[3]} MY$end"
+# # echo "NOOOO it is the worst"
+
+echo -e "${array_color[0]}${array_color[1]}HOSTNAME$fin        = ${array_color[2]}${array_color[3]}$HOSTNAME $fin"
+echo -e "${array_color[0]}${array_color[1]}TIMEZONE$fin        = ${array_color[2]}${array_color[3]}$TIMEZONE $fin"
+echo -e "${array_color[0]}${array_color[1]}OS$fin              = ${array_color[2]}${array_color[3]}$OS $fin"
+echo -e "${array_color[0]}${array_color[1]}DATE$fin            = ${array_color[2]}${array_color[3]}$DATE $fin"
+echo -e "${array_color[0]}${array_color[1]}UPTIME$fin          = ${array_color[2]}${array_color[3]}$UPTIME $fin"
+echo -e "${array_color[0]}${array_color[1]}UPTIME_SEC$fin      = ${array_color[2]}${array_color[3]}$UPTIME_SEC $fin"
+echo -e "${array_color[0]}${array_color[1]}IP$fin              = ${array_color[2]}${array_color[3]}$IP $fin"
+echo -e "${array_color[0]}${array_color[1]}MASK$fin            = ${array_color[2]}${array_color[3]}$MASK $fin"
+echo -e "${array_color[0]}${array_color[1]}GATEWAY$fin         = ${array_color[2]}${array_color[3]}$GATEWAY $fin"
+echo -e "${array_color[0]}${array_color[1]}RAM_TOTAL$fin       = ${array_color[2]}${array_color[3]}$RAM_TOTAL $fin"
+echo -e "${array_color[0]}${array_color[1]}RAM_USED$fin        = ${array_color[2]}${array_color[3]}$RAM_USED $fin"
+echo -e "${array_color[0]}${array_color[1]}RAM_FREE$fin        = ${array_color[2]}${array_color[3]}$RAM_FREE $fin"
+echo -e "${array_color[0]}${array_color[1]}SPACE_ROOT$fin      = ${array_color[2]}${array_color[3]}$SPACE_ROOT $fin"
+echo -e "${array_color[0]}${array_color[1]}SPACE_ROOT_USED$fin = ${array_color[2]}${array_color[3]}$SPACE_ROOT_USED $fin"
+echo -e "${array_color[0]}${array_color[1]}SPACE_ROOT_USED$fin = ${array_color[2]}${array_color[3]}$SPACE_ROOT_USED$end $fin"
