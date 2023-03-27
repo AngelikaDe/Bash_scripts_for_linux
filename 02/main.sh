e95e2177448dbc5e78517a25c19316e90d11f525
Анжелика Демина <angelika@192.168.1.3>
2023-03-28 01:31:36 +0300

    02

--- 02/main.sh
+++ 02/main.sh
@@ -8,7 +8,23 @@ exit 1
 fi
 chmod +x ./info.sh
 ./info.sh $1
-echo "$(sh info.sh)"
+echo "HOSTNAME = $HOSTNAME"
+echo "TIMEZONE = $TIMEZONE"
+echo "USER = $USER"
+echo "OS = $OS"
+echo "DATE = $DATE"
+echo "UPTIME = $UPTIME"
+echo "UPTIME_SEC = $UPTIME_SEC"
+echo "IP = $IP"
+echo "MASK = $MASK"
+echo "GATEWAY = $GATEWAY"
+echo "RAM_TOTAL = $RAM_TOTAL"
+echo "RAM_USED = $RAM_USED"
+echo "RAM_FREE = $RAM_FREE"
+echo "SPACE_ROOT = $SPACE_ROOT"
+echo "SPACE_ROOT_USED = $SPACE_ROOT_USED"
+echo "SPACE_ROOT_FREE = $SPACE_ROOT_FREE"
+
 
 file_name="$(date "+%d_%m_%Y_%H_%M_%S").ststus" 
 read -p "Do you want to save the information? [Y/N]" choice
